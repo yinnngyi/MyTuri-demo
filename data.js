66f474c90077434e5f497131027db11bfff8013b
Nini <58293146+yinnngyi@users.noreply.github.com>
2025-08-22 22:16:22 +0800

    test file upload

---
+++ data.js
@@ -0,0 +1,208 @@
+let testData = {
+    metaTitle: '康健會員週報',
+    newsletterLink: 'https://www.commonhealth.com.tw',
+    newsletterType: 'subscription',
+    quoteText: '「當醫生開始把病人看作是收入的來源，而不是服務的對象時，醫療的靈魂就迷失了。」',
+    quoteSource: '——美國哈佛醫學院教授理查德·卡巴特（Richard Cabot） ',
+    sections: [
+        { type: 'contentTitle', content: '這是一封測試信件，內容是假文' },
+        {
+            type: 'contentGreenTitle',
+            content: '親愛的康健夥伴，您好：'
+        },
+        {
+            type: 'contentText',
+            content: '親愛的讀者，你好：<br><br>內文文字內文文字，內文文字<a href="https://www.commonhealth.com.tw/" target="_blank" style="font-family: &quot;Microsoft JhengHei&quot;, 微軟正黑體, &quot;Microsoft YaHei&quot;, sans-serif; font-size: 18px; line-height: 1.6; color: rgb(0, 153, 68); text-decoration: underline;">連結</a>內文文字。報稅的季節將近，你有算算過去一年的收入中，有多少用在醫療支出嗎？<br><br>我有個長官兩年前去開了白內障，聽從醫師的建議選擇自費人工水晶體水晶體水晶體水晶體，雙眼花了18萬元後，覺得眼前大放光明，「還好有保險，」他說。但不到一年時間，老花就恢復原樣你也有抗老的祕訣嗎？<br><br>原以為這數字已經很驚人，原以為這數字已經很驚人，日前另一位朋友也去開白內障，一隻眼睛就要13萬元，令人瞠目結舌。'
+        },
+        {
+            type: 'contentImage',
+            imageUrl: 'https://ci3.googleusercontent.com/meips/ADKq_NYzJDCiU5NgjtJPysxdQjyCJy3adkICFVG16sIEInCnhVS_IY0bAsay27pTfyqPtxXXSnXwQK1m9rfZJvm8foag14nH2pKw69ga5y1tVZ_RVv6i2Lbo38NPrQ=s0-d-e1-ft#https://web.commonhealth.com.tw/edm/member/202505/0506/images/03.jpg',
+            imageLink: 'https://www.commonhealth.com.tw',
+            imageW: '99999',
+        },
+        {
+            type: 'contentText',
+            content: '「這個窘境，也不光發生在台灣。因此，今年國際護師節（母親節隔天的5/12)倡議主題，就是「有護理師才有未來：關懷護理師促進經濟成長(Our Nurses. Our Future. Caring for nurses strengthens economies) 」，強調護理人員的身心健康，攸關護理品質與全球醫療體系的韌性！'
+        },
+        {
+            type: 'contentText',
+            content: '我很贊成「nurses strengthens economies」的積極，但這句若改成「Nurses strengthened economies」就有了更包容的意義：經濟的成長，正是因為有了「母親」軟實力的愛心與耐心照顧！'
+        },
+        {
+            type: 'contentImage',
+            imageUrl: 'https://web.commonhealth.com.tw/edm/chweekly/202504/0426/images/313cover.jpg',
+            imageLink: 'https://www.commonhealth.com.tw',
+            imageW: '200',
+            imageText: '這行是圖片敘述，自費就醫指南',
+        },
+        {
+            type: 'contentText',
+            content: '因為，就像林美琪護理師說的：「我很喜歡護理工作。護理師能夠幫助到一整個家庭，讓人在慌亂中獲得支持、很快獲得喘息、安定下來，就像抓到一根浮木一樣。」<br><br>歡迎 <a href="mailto:cheditor@cw.com.tw" data-no-track="true" alias="no-track" data-msys-clicktrack="0" style="font-family: &quot;Microsoft JhengHei&quot;, 微軟正黑體, &quot;Microsoft YaHei&quot;, sans-serif; font-size: 18px; line-height: 1.6; color: rgb(0, 153, 68); text-decoration: underline;">寫信告訴我</a>。'
+        }
+    ],
+    signatureImg: 'https://as.chdev.tw/club/ckeditor/202404/ckeditor-662b50da94676.jpg',
+    signatureText: '《康健》董事長',
+    articleMainLink: 'https://www.commonhealth.com.tw',
+    articleMainImageUrl: 'https://as.chdev.tw/web/article/photos/1/7/4/ee1d7eabac62dd933fbd0590a718516e27a6d244.jpg',
+    articleMainTitle: '養生，從喝水開始！',
+    articleMainText: '每天喝水看似簡單，卻大有學問！補對水能助代謝、護心腎，還能提升專注力。破解喝水迷思，讓健康從一杯水開始。',
+    articleGroups: [
+        {
+            groupTitle: '沒事多喝水',
+            articles: [
+                {
+                    link: 'https://www.commonhealth.com.tw',
+                    imageUrl: 'https://as.chdev.tw/web/article/a/5/4/a3d47f87-57ec-4951-899f-dad298d15a531642486248.jpg',
+                    title: '別等渴了才喝水　專家：水喝不夠老年失智、譫妄風險增',
+                    text: '喝水是日常小事，卻是老年人健康大事，天冷時老人家常因不感覺口渴減少喝水，但長者水分攝取不足有失智風險、引發譫妄，還容易泌尿道感染。',
+                    more: '閱讀全文',
+                },
+                {
+                    link: 'https://www.commonhealth.com.tw',
+                    imageUrl: 'https://as.chdev.tw/web/article/a/0/4/ad756025-0996-4189-af44-82b6f43bd4bb1745552267.webp',
+                    title: '請問康健》缺水小心疾病找上身！名醫教你喝水養生',
+                    text: '水可說是生命的基礎，也是人體內含量最多的成分，但大多數人卻沒有好好喝水，當體內缺乏水份時，往往容易產生組織損傷、疼痛等各種健康問題，嚴重時還會引發許多疾病與症狀。到底喝水有什麼學問？如何喝水養生？',
+                    more: '閱讀全文',
+                }
+            ]
+        },
+        {
+            groupTitle: '健康知識',
+            articles: [
+                {
+                    link: 'https://www.commonhealth.com.tw',
+                    imageUrl: 'https://as.chdev.tw/web/article/3/4/4/374e47e5-41d8-4387-be8d-d99778b0e8381709897983.jpg',
+                    title: '小腹很凸、下背痠痛... 「骨盆前傾」其實靠3睡姿、3運動就能改善',
+                    text: '骨盆前傾可能與坐姿不良有關，會導致小腹凸起、下背痠痛與筋腿受傷。避免骨盆前傾怎麼做？骨盆前傾如何治療？一次學會運動訓練及矯正方式。',
+                    more: '閱讀全文',
+                },
+                {
+                    link: 'https://www.commonhealth.com.tw',
+                    imageUrl: 'https://ch-image-resizer.cwg.tw/resize/uri/https%3A%2F%2Fch-cancer-cdn.cwg.tw%2Farticle%2F202403%2Farticle-65efea69b3aca.jpg/?format=webp',
+                    title: '手術隔天就出院！「腫瘤冷凍消融術」傷口小、復原快，適合7類癌症患者',
+                    text: '冷凍消融的侵入性極小，溫度低對患者來說幾乎無痛，患者手術完平躺 4 小時左右，隔天就可以出院。 哪些人適用？手術如何進行？一次看懂。',
+                    more: '閱讀全文',
+                }
+            ]
+        },
+        {
+            groupTitle: '飲食健康',
+            articles: [
+                {
+                    link: 'https://www.commonhealth.com.tw',
+                    imageUrl: 'https://web.commonhealth.com.tw/edm/chweekly/202309/0909/images/09093.png',
+                    title: '咖啡、茶、紅酒，誰才是「抗老」王者？',
+                    text: '想抗老不只靠保養品，日常飲品也能大大助力！咖啡提神又抗氧化，綠茶富含多酚守護健康，紅酒適量飲用更有助心血管保健。喝對讓你從內到外延緩老化！',
+                    more: '閱讀全文',
+                }
+            ]
+        },
+        {
+            groupTitle: '本週精選',
+            articles: [
+                {
+                    link: 'https://www.commonhealth.com.tw',
+                    imageUrl: 'https://as.chdev.tw/web/article/a/f/4/a9a7511f-fa20-4dad-aa1c-b2021693f5731709892110.jpg',
+                    title: '夏季疫情竄升，分不清是感冒還新冠？醫師：流感已結束，有這些症狀就是新冠',
+                    text: '老人家跌倒＝死亡將近？ 熟齡跌倒4部位常骨折，這處更是「致命隱憂」跌倒是高齡族群常發生的意外傷害，老人家一跌不起更時有所聞，老人跌倒為什麼這麼嚴重？',
+                    more: '閱讀全文',
+                }
+            ]
+        },
+    ],
+    podcastImg: 'https://web.commonhealth.com.tw/edm/chweekly/components/podcast-img.webp',
+    podcastGreenTitle: '聽康健 Podcast',
+    podcastCategory: '健康保健',
+    podcastTitle: '養生，從喝水開始！我愛喝水，沒事多喝水，原來還可以這樣做',
+    podcastLink: 'https://as.chdev.tw/web/article/photos/1/7/4/7dc7af2fc8f205eb834dc6ce60694a05baac16f6.jpg',
+    ADBanner: [
+        {
+            imageUrl: 'https://as.chdev.tw/web/article/photos/1/7/4/7dc7af2fc8f205eb834dc6ce60694a05baac16f6.jpg',
+            link: 'https://www.commonhealth.com.tw',
+        },
+    ],
+    ADLeftImg: 'https://tpc.googlesyndication.com/simgad/1937753754622538615',
+    ADLeftLink: 'https://www.commonhealth.com.tw',
+    ADRightImg: 'https://as.chdev.tw/web/article/6/3/4/63a83e09-3a9b-418c-b630-481f9c8b39341747379088.webp',
+    ADRightLink: 'https://www.commonhealth.com.tw',
+}
+
+
+let emptyData = {
+    metaTitle: '康健會員週報',
+    newsletterLink: 'https://www.commonhealth.com.tw',
+    newsletterType: 'subscription',
+    quoteText: '',
+    quoteSource: '',
+    sections: [
+        { type: 'contentTitle', content: '標題' },
+    ],
+    signatureImg: 'https://as.chdev.tw/club/ckeditor/202404/ckeditor-662b50da94676.jpg',
+    signatureText: '《康健》董事長',
+    articleMainLink: '',
+    articleMainImageUrl: '',
+    articleMainTitle: '',
+    articleMainText: '',
+    articleGroups: [
+        {
+            groupTitle: '',
+            articles: [
+                {
+                    link: '',
+                    imageUrl: '',
+                    title: '',
+                    text: '',
+                    more: '閱讀全文',
+                },
+            ]
+        }, {
+            groupTitle: '',
+            articles: [
+                {
+                    link: '',
+                    imageUrl: '',
+                    title: '',
+                    text: '',
+                    more: '閱讀全文',
+                },
+            ]
+        }, {
+            groupTitle: '',
+            articles: [
+                {
+                    link: '',
+                    imageUrl: '',
+                    title: '',
+                    text: '',
+                    more: '閱讀全文',
+                },
+            ]
+        }, {
+            groupTitle: '',
+            articles: [
+                {
+                    link: '',
+                    imageUrl: '',
+                    title: '',
+                    text: '',
+                    more: '閱讀全文',
+                },
+            ]
+        },
+    ],
+    podcastImg: 'https://web.commonhealth.com.tw/edm/chweekly/components/podcast-img.webp',
+    podcastGreenTitle: '聽康健 Podcast',
+    podcastCategory: '',
+    podcastTitle: '',
+    podcastLink: '',
+    ADBanner: [
+        {
+            imageUrl: '',
+            link: '',
+        },
+    ],
+    ADLeftImg: '',
+    ADLeftLink: '',
+    ADRightImg: '',
+    ADRightLink: '',
+}
